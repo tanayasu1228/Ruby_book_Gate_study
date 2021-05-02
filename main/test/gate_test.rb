9aa5728d9b0fe07a9b9a4e733c8a11217511da23
tanahashi.yasuhiro <yasuhiro.tanahashi@vitalize.co.jp>
2021-05-02 22:11:13 +0900

    テスト３．４

--- main/test/gate_test.rb
+++ main/test/gate_test.rb
@@ -8,7 +8,7 @@ class GateTest < Minitest::Test
     @juso = Gate.new(:juso)
     @mikuni = Gate.new(:mikuni)
   end
-  
+
   def test_umeda_to_juso
     ticket = Ticket.new(150)
     @umeda.enter(ticket)
@@ -19,5 +19,17 @@ class GateTest < Minitest::Test
     ticket = Ticket.new(150)
     @umeda.enter(ticket)
     refute @mikuni.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
